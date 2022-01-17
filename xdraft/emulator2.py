520e36d71683c4d51bfa35726b8fa73973ee2d29
Iris Kutsyy <ak2149@cam.ac.uk>
2022-01-17 23:25:01 +0000

    fixed parallelization bug

--- xdraft/emulator2.py
+++ xdraft/emulator2.py
@@ -12,92 +12,91 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pickle
 
-train = True
-if train:
-    space = ParameterSpace([ContinuousParameter('traffic_light_1', 1, 4),
-                                    ContinuousParameter('traffic_light_2', 1, 4),
-                                    ContinuousParameter('traffic_light_3', 1, 4),
-                                    ContinuousParameter('traffic_light_4', 1, 4),
-                                    ContinuousParameter('trucks', 1, 1),
-                                    ContinuousParameter('cars', 1, 2),
-                                    ContinuousParameter('bikes', 1, 1),
-                                    ContinuousParameter('Heaviness',0,1),
-                                    ContinuousParameter('NB', 0.1, 0.5),
-                                    ContinuousParameter('EB', 0.1, 0.5),
-                                    ContinuousParameter('SB', 0.1, 0.5),
-                                    ContinuousParameter('WB', 0.1, 0.5)
-                                    ])
-    num_data_points = 30
-    f = runner.call_sim_parallel
-    design = RandomDesign(space)
-    X = design.get_samples(num_data_points)
-    print(X)
-    Y = np.array(runner.call_sim_parallel(X))
-    print(Y)
-    kern = GPy.kern.RBF(21, lengthscale=0.08, variance=20)
-    model_gpy = GPRegression(X,Y, kern, noise_var=1e-10)
-    model_gpy.optimize()
-    model_emukit = GPyModelWrapper(model_gpy)
-    model_variance   = ModelVariance(model = model_emukit)
-    optimizer        = GradientAcquisitionOptimizer(space = space)
-    expdesign_loop = ExperimentalDesignLoop(space = space,
-                                            model = model_emukit,
-                                            acquisition = model_variance,
-                                            update_interval = 1,
-                                            batch_size = 5)
+if __name__ == '__main__':
+
+    train = True
+    if train:
+        space = ParameterSpace([ContinuousParameter('traffic_light_1', 1, 4),
+                                        ContinuousParameter('traffic_light_2', 1, 4),
+                                        ContinuousParameter('traffic_light_3', 1, 4),
+                                        ContinuousParameter('traffic_light_4', 1, 4),
+                                        ContinuousParameter('trucks', 1, 1),
+                                        ContinuousParameter('cars', 1, 2),
+                                        ContinuousParameter('bikes', 1, 1),
+                                        ContinuousParameter('Heaviness',0,1),
+                                        ContinuousParameter('NB', 0.1, 0.5),
+                                        ContinuousParameter('EB', 0.1, 0.5),
+                                        ContinuousParameter('SB', 0.1, 0.5),
+                                        ContinuousParameter('WB', 0.1, 0.5)
+                                        ])
+        num_data_points = 30
+        f = runner.call_sim_parallel
+        design = RandomDesign(space)
+        X = design.get_samples(num_data_points)
+        print(X)
+        Y = np.array(runner.call_sim_parallel(X))
+        print(Y)
+        kern = GPy.kern.RBF(21, lengthscale=0.08, variance=20)
+        model_gpy = GPRegression(X,Y, kern, noise_var=1e-10)
+        model_gpy.optimize()
+        model_emukit = GPyModelWrapper(model_gpy)
+        model_variance   = ModelVariance(model = model_emukit)
+        optimizer        = GradientAcquisitionOptimizer(space = space)
+        expdesign_loop = ExperimentalDesignLoop(space = space,
+                                                model = model_emukit,
+                                                acquisition = model_variance,
+                                                update_interval = 1,
+                                                batch_size = 5)
 
 
-    # Run the loop
-    stopping_condition = FixedIterationsStoppingCondition(i_max = 250)
-    expdesign_loop.run_loop(f, stopping_condition)
+        # Run the loop
+        stopping_condition = FixedIterationsStoppingCondition(i_max = 250)
+        expdesign_loop.run_loop(f, stopping_condition)
 
 
-    with open('model.pkl','wb') as pw:
-        pickle.dump(model_emukit,pw)
-        print("Dumped model succesfully")
+        with open('model.pkl','wb') as pw:
+            pickle.dump(model_emukit,pw)
+            print("Dumped model succesfully")
 
-else:
-    with open('model.pkl','rb') as pw:
-        model_emukit = pickle.load(pw)
-        print("loaded model")
+    else:
+        with open('model.pkl','rb') as pw:
+            model_emukit = pickle.load(pw)
+            print("loaded model")
 
 
 
-#Get some sample vals for plotting how it does
-#For all these, assume everything bar traffic light #1 is fixed, i.e.:
+    #Get some sample vals for plotting how it does
+    #For all these, assume everything bar traffic light #1 is fixed, i.e.:
 
-fixed_params = [3,3,3,0.5,0.5, 1, 2, 1, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3]
-#Then pick values for the 4 traffic light times:
-xs = []
-for i in range(1,44,1):
-    k = fixed_params.copy()
-    k.insert(0,i/4)
-    xs.append(k)
+    fixed_params = [3,3,3,0.5,0.5, 1, 2, 1, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3]
+    #Then pick values for the 4 traffic light times:
+    xs = []
+    for i in range(1,44,1):
+        k = fixed_params.copy()
+        k.insert(0,i/4)
+        xs.append(k)
 
-print(xs)
+    print(xs)
 
-predicted_y = []
-predicted_std = []
-for x in xs:
-    y, var = model_emukit.predict(np.array([x]))
-    std = np.sqrt(var)
-    predicted_y.append(y)
-    predicted_std.append(std)
+    predicted_y = []
+    predicted_std = []
+    for x in xs:
+        y, var = model_emukit.predict(np.array([x]))
+        std = np.sqrt(var)
+        predicted_y.append(y)
+        predicted_std.append(std)
 
-predicted_y = np.array(predicted_y).flatten()
-print(predicted_y)
-predicted_std = np.array(predicted_std).flatten()
-#real_ys = runner.call_sim_parallel(xs)
+    predicted_y = np.array(predicted_y).flatten()
+    print(predicted_y)
+    predicted_std = np.array(predicted_std).flatten()
+    #real_ys = runner.call_sim_parallel(xs)
 
-plt.title('Learning function sin(x) with Emukit')
-plt.xlabel('x')
-plt.ylabel('y', rotation=None)
-xs = list(map(lambda x: x/4, range(1,44,1)))
-#plt.plot(xs, real_ys, c='r', )
-plt.plot(xs, predicted_y)
-plt.legend(['True function', 'Estimated function'], loc='lower right')
-plt.fill_between(xs, predicted_y - 2 * predicted_std, predicted_y + 2 * predicted_std, alpha=.5)
-plt.show()
-
-if __name__ == "__main__":
-    print('fish')
+    plt.title('Learning function sin(x) with Emukit')
+    plt.xlabel('x')
+    plt.ylabel('y', rotation=None)
+    xs = list(map(lambda x: x/4, range(1,44,1)))
+    #plt.plot(xs, real_ys, c='r', )
+    plt.plot(xs, predicted_y)
+    plt.legend(['True function', 'Estimated function'], loc='lower right')
+    plt.fill_between(xs, predicted_y - 2 * predicted_std, predicted_y + 2 * predicted_std, alpha=.5)
+    plt.show()
--- xdraft/runner.py
+++ xdraft/runner.py
@@ -1,14 +1,12 @@
-if __name__ == '__main__':
-    from sumolib import checkBinary  # noqa
-    import traci
-    import pandas as pd
-    import matplotlib.pyplot as plt
-    from scipy import integrate
-    import xml.etree.ElementTree as ET
-    import os
-    import numpy as np
-    import multiprocessing as mp
-
+from sumolib import checkBinary  # noqa
+import traci
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy import integrate
+import xml.etree.ElementTree as ET
+import os
+import numpy as np
+import multiprocessing as mp
 
 cccc = 0
 
@@ -19,8 +17,7 @@ METRIC = 'meanSpeedRelative'
 GRIDLOCK_PENALTY = 0.1
 
 N_LIMIT = 3600
-N = 1000
-N_reweight = 1.5
+N = 2000
 
 NO_SIM = True  # skip re-simulating
 
@@ -31,9 +28,9 @@ else:
     sumoBinary = checkBinary('sumo-gui')
 
 
-#MAIN FUNCTION TO CALL SIMULATOR
-#Input: params - 21 length long vector - definition below
-#Returns: Health metric depending on that set above. A higher value for this is better.
+# MAIN FUNCTION TO CALL SIMULATOR
+# Input: params - 21 length long vector - definition below
+# Returns: Health metric depending on that set above. A higher value for this is better.
 
 def call_sim_parallel(dparams):
     global cccc
@@ -49,26 +46,26 @@ def call_sim_zipped(zipped):
     return call_sim(*zipped)
 
 
-def call_sim(params, name='test',network_type='intersection', early_stop=False):
+def call_sim(params, name="test", network_type='intersection', early_stop=False):
     ret_code = update_network(params,name)
     if not ret_code:
         return 0.0
+    update_sumoconfig(name)
     grid_pen = go(name)
     grid_pen = grid_pen * GRIDLOCK_PENALTY
-    print(params)
     return get_sum_stats(grid_pen, name)
-
 
 def get_sum_stats(grid_pen, name, metric='meanSpeedRelative'):
     # metric = one of 'halting-ratio', 'meanTravelTime' or 'meanSpeedRelative'
     df = pd.read_xml("in_use_files/sum_" + str(name) + ".xml")[:-1]
-    df = df.head(int(len(df)*0.9)).tail(int(len(df)*0.8))
+    df = df.head(int(len(df) * 0.9)).tail(int(len(df) * 0.8))
     if metric == 'halting-ratio':
         df['halting-ratio'] = df['halting'] / df['running']
     metric_out = np.average(df[metric])
-    print(metric_out, " - ", grid_pen, " = ", metric_out-grid_pen)
+    print(metric_out, " - ", grid_pen, " = ", metric_out - grid_pen)
 
-    return metric_out
+    return max(0, metric_out - grid_pen)
+
 
 def set_session():
     global session
@@ -90,91 +87,121 @@ def go(name):
     while conn.simulation.getMinExpectedNumber() > 0:
         conn.simulationStep()
         iterations += 1
-        if iterations % 300 == 0:
+        if iterations % 500 == 0:
             cur_inb_sp = conn.edge.getLastStepMeanSpeed("end1_junction")
             cur_outb_sp = conn.edge.getLastStepMeanSpeed("junction_end1")
 
             if (cur_inb_sp == last_inb_sp and cur_outb_sp == last_outb_sp):
-                print("Gridlock! Breaking...")
+                print("Traffic Stabilized. Breaking...")
                 conn.close()
-                return 1
+                return 0
             else:
                 last_outb_sp = cur_outb_sp
                 last_inb_sp = cur_inb_sp
+    print("---------------------"+str(iterations))
     conn.close()
     return 0
 
 
 # total is total traffic flow on edge, ratios is ratio of proportions
 # e.g. total flow of 10 and truck:car:bike ratio of 5:3:2 = 5 trucks, 3 cars, 2 bikes
-#selected is which ratio element (by value)
+# selected is which ratio element (by value)
 def get_props_from_total(total, ratios, selected):
-    return str(round(total * selected / sum(ratios),5))
+    return str(round(total * selected / sum(ratios), 5))
 
+def update_sumoconfig(name):
+    with open('./inter1.sumocfg','r') as f:
+        s = f.read()
+        s = s.replace("draft",str(name))
+        with open("./in_use_files/"+str(name)+".sumocfg",'w+') as f2:
+            f2.write(s)
 
 def update_network(param_list, savename='test'):
     # Must have all routes flowing to stop it falling into just allowing the busiest lane to flow only.
     if 0 in param_list[0:4]:
         return False
 
-    """    space = ParameterSpace([ContinuousParameter('traffic_light_1', 1, 4),   0 
-                                    ContinuousParameter('traffic_light_2', 1, 4), 1
-                                    ContinuousParameter('traffic_light_3', 1, 4),2
-                                    ContinuousParameter('traffic_light_4', 1, 4),3
-                                    ContinuousParameter('trucks', 1, 1),4
-                                    ContinuousParameter('cars', 1, 2),5
-                                    ContinuousParameter('bikes', 1, 1),6
-                                    ContinuousParameter('heaviness',0,1),7
-                                    ContinuousParameter('NB', 0, 1),8
-                                    ContinuousParameter('EB', 0, 1),9
-                                    ContinuousParameter('SB', 0, 1),10
-                                    ContinuousParameter('WB', 0, 1) 11
-                                    ])
-                                    """
-    #print(list(zip(param_list,range(0,len(param_list)))))
+    # Mock param list:
+    try:
+        assert len(param_list) == 21
+    except:
+        print("PARAMLIST:", param_list)
 
-    with open(os.path.join(os.path.dirname(__file__), './draft.rou.xml')) as f:
+    """
+    Params 0 - 3 = Traffic light setups - ratio values (are normalised before use!)
+            Default phases are
+            0: N->S and S->N
+            1: E->W and W->E
+            2: N->E and S->W
+            3: W->N and E->S
+    Params 4, 5 = Sigma and tau - see Krauss Model -  https://sumo.dlr.de/docs/Definition_of_Vehicles%2C_Vehicle_Types%2C_and_Routes.html
+    Params 6, 7, 8 = Ratio of trucks (6) , cars (7) , bikes (8) (as whole-number ratios/parts).
+    Params 9 to 20 (including 20) are matrix of flow probabilities for each source->dest; skipping the leading diagonal.
+    Flow probability = probability of a vehicle being emitted in any one second, yields binomial -
+    see: https://sumo.dlr.de/docs/Simulation/Randomness.html#flows_with_a_random_number_of_vehicles
+    NB: Not modelled per lane so can have duplicate spawns of vehicle types; sumo handles these rare cases by dropping extra vehicles.
+        (NB in the xml files, SUMO uses 1=North 2=East 3=West 4=South)
+        9: N->E (i.e. 1->2) 10: N->S  11: N->W
+        12: E->N  13: E->S  14: E->W
+        15: S->N  16: S->E  17: S->W
+        18: W->N  19: W->E  20: W->S
+
+    """
+    # print(list(zip(param_list,range(0,len(param_list)))))
+
+    with open('./draft.rou.xml') as f:
         root = ET.parse(f)
 
-        heaviness = param_list[7] * N
-        traffs = param_list[4:7]
-        ps=param_list[8:]
-        normalised_traffs = list(x/sum(traffs) for x in traffs)
-
-        #so truck_ps[0] is flow for any incoming edge, for the going NORTH
-
         # Update probability of emissions ( = flow):
+        flowdown = min(N_LIMIT, int(N / sum(param_list[9:])))
         for i, n in enumerate(root.iter('flow')):
+            # i is the index of the specific ((source,dest),vehicle). There are 3 vehicle types.
+            # k is the index into the parameters for the specific source->dest. These do not distinguish by vehicle,
+            # so there are a third of the overall number of flows (i). So i = 3k (trucks) or 3k + 1 (cars) or 3k+2 (bikes)
+            k = i // 3
 
-            source_direction = i // 9
-
-            #0: truck, east
-            #1: truck, south
-            #3: truck, west
-            #4: car, east
-
-            #List of what probability to send E/S/W
-            normalised_flows = get_normalised_flows(source_direction,ps)
-
-            veh_prop = normalised_traffs[(i % 9) //3]
-            n.set('probability',str(round(veh_prop*normalised_flows[i % 3],5)))
-            n.set('end', str(heaviness))
+            # First arg - the overall flow (sum of all vehicle types) which should be assigned to the flow. This is what
+            # is passed in as a parameter for the specific flow - add 9 as this starts from index 9.
+            # Second arg - the ratio values for the three types of vehicle.
+            # Third arg - the ratio value for whichever of the vehicle types are currently being assigned - add 6 as this
+            # starts form index 6. i - 3k = 1 or 2 or 3 i.e. the vehicle type.
+            prob = get_props_from_total(param_list[9 + k], param_list[6:9], param_list[6 + i - (3 * k)])
+            n.set('probability', prob)
+            n.set('end', str(flowdown))
             # print(9+k, prob, param_list[9+k],n.attrib)
-        root.write("./in_use_files/" + str(savename)+".rou.xml")
 
-    #Update the traffic lights
-    with open(os.path.join(os.path.dirname(__file__), './draft.net.xml')) as f:
+        # Set tau and sigma for all vTypes
+        # for vType in root.findall('vType'):
+        # print()
+        # vType.set('sigma',str(param_list[4]))
+        # vType.set('tau',str(param_list[5]))
+
+        root.write("./in_use_files/" + str(savename) + ".rou.xml")
+
+    # Update the traffic lights
+    with open('./draft.net.xml') as f:
         root = ET.parse(f)
         zs = list(x for x in root.find('tlLogic') if 'y' not in x.get('state'))
         for i, phase in enumerate(zs):
-            #print(phase.attrib, param_list[i], get_props_from_total(1,param_list[0:4],param_list[i]))
-            phase.set('duration', get_props_from_total(60,param_list[0:4],param_list[i]))
-        root.write("./in_use_files/" + str(savename)+".net.xml")
+            # print(phase.attrib, param_list[i], get_props_from_total(1,param_list[0:4],param_list[i]))
+            phase.set('duration', get_props_from_total(60, param_list[0:4], param_list[i]))
+        root.write("./in_use_files/" + str(savename) + ".net.xml")
 
     return True
 
-#E.g. pass excl = 0 for traffic coming from the north, will leave out the NB part of flow.
-def get_normalised_flows(excl, ps):
-    ps2 = list(ps)
-    del ps2[excl]
-    return list(x / sum(ps2) for x in ps2)
+
+# this is the main entry point of this script
+if __name__ == "__main__":
+    update_sumoconfig("test")
+    go("test")
+    get_sum_stats("test")
+    """
+    # this script has been called from the command line. It will start sumo as a
+    # server, then connect and run
+    if  NO_SIM: go()
+    param_list = [99, 3, 106, 12,
+                  0.5, 0.5,
+                  1, 1, 1,
+                  0.9, 0.9, 0.9, 0.9, 0.9, 0.45, 0.45, 0.45, 0.1, 0.1, 0.1, 0.1]
+    update_network(param_list)
+    go()"""
