c239bbbb0d788b1e0065a9637ed29ec2104f1715
JoshC8C7 <joshuacowan14@gmail.com>
2022-01-17 03:23:25 +0000

    bug fixes

--- xdraft/runner.py
+++ xdraft/runner.py
@@ -5,13 +5,16 @@ import matplotlib.pyplot as plt
 from scipy import integrate
 import xml.etree.ElementTree as ET
 import os
+import numpy as np
 
-global session
-session = False
-NO_GUI = True
+
+NO_GUI = False
 TRAFFIC_TYPES = ['car1', 'truck1', 'bike1']
 pd.set_option('display.max_columns', None)
 METRIC = 'meanSpeedRelative'
+
+N_LIMIT = 600
+N = 1500
 
 NO_SIM = True  # skip re-simulating
 
@@ -25,8 +28,12 @@ else:
 #MAIN FUNCTION TO CALL SIMULATOR
 #Input: params - 21 length long vector - definition below
 #Returns: Health metric depending on that set above. A higher value for this is better.
+
+def call_sim_parallel(dparams):
+    return np.array(list([call_sim(x)] for x in dparams))
+
 def call_sim(params, network_type='intersection', early_stop=False):
-    ret_code = update_network(network_type, params)
+    ret_code = update_network(params)
     if not ret_code:
         return 0.0
     go(early_stop=early_stop)
@@ -38,9 +45,9 @@ def get_sum_stats(metric='meanSpeedRelative'):
     df = pd.read_xml("sum.xml")[:-1]
     if metric == 'halting-ratio':
         df['halting-ratio'] = df['halting'] / df['running']
-    # df.plot(x="ended",y=metric)
-    # plt.show()
-    metric_out = int(int(integrate.trapezoid(df['ended'], df[metric])) / df['ended'].values[-1])
+    #df.plot(x="ended",y=metric)
+    #plt.show()
+    metric_out = abs(int(1000*integrate.trapezoid(df['ended'], df[metric])) / df['ended'].values[-1])
     return metric_out
 
 def set_session():
@@ -48,9 +55,10 @@ def set_session():
     session = True
 
 def go(early_stop = False):
-    os.remove("sum.xml")
+    if os.path.isfile("sum.xml"):
+        os.remove("sum.xml")
     cmd = [sumoBinary, "-c", "inter1.sumocfg", "--start", "--quit-on-end",
-           "--summary", "sum.xml", "--tripinfo-output", "tripinfo.xml", "--no-warnings"]
+           "--summary", "sum.xml", "--tripinfo-output", "tripinfo.xml"]
     traci.start(cmd)
     iterations = 0
     while traci.simulation.getMinExpectedNumber() > 0:
@@ -67,17 +75,16 @@ def get_props_from_total(total, ratios, selected):
     return str(round(total * selected / sum(ratios),5))
 
 
-def update_network(network_type, param_list):
+def update_network(param_list):
     # Must have all routes flowing to stop it falling into just allowing the busiest lane to flow only.
     if 0 in param_list[0:4]:
         return False
 
     #Mock param list:
-    #param_list = [99,3,106,12,
-    #              0.5,0.5,
-    #             1,1,1,
-    #              0.9,0.9,0.9,0.9,0.9,0.45,0.45,0.45,0.1,0.1,0.1,0.1]
-    assert len(param_list) == 21
+    try:
+        assert len(param_list) == 21
+    except:
+        print("PARAMLIST:", param_list)
 
     """
     Params 0 - 3 = Traffic light setups - ratio values (are normalised before use!)
@@ -99,12 +106,13 @@ def update_network(network_type, param_list):
         18: W->N  19: W->E  20: W->S
 
     """
-    print(list(zip(param_list,range(0,len(param_list)))))
+    #print(list(zip(param_list,range(0,len(param_list)))))
 
     with open(os.path.join(os.path.dirname(__file__), './draft.rou.xml')) as f:
         root = ET.parse(f)
 
         # Update probability of emissions ( = flow):
+        flowdown = min(N_LIMIT, int(N/sum(param_list[9:])))
         for i, n in enumerate(root.iter('flow')):
             # i is the index of the specific ((source,dest),vehicle). There are 3 vehicle types.
             # k is the index into the parameters for the specific source->dest. These do not distinguish by vehicle,
@@ -118,6 +126,7 @@ def update_network(network_type, param_list):
             #starts form index 6. i - 3k = 1 or 2 or 3 i.e. the vehicle type.
             prob = get_props_from_total(param_list[9+k],param_list[6:9],param_list[6+i-(3*k)])
             n.set('probability',prob)
+            n.set('end', str(flowdown))
             #print(9+k, prob, param_list[9+k],n.attrib)
 
         #Set tau and sigma for all vTypes
@@ -139,11 +148,15 @@ def update_network(network_type, param_list):
 
 # this is the main entry point of this script
 if __name__ == "__main__":
-
+    go()
+    get_sum_stats()
+    """
     # this script has been called from the command line. It will start sumo as a
     # server, then connect and run
-
     if  NO_SIM: go()
-    update_network('intersection',[])
-    go()
-    traci.close()
+    param_list = [99, 3, 106, 12,
+                  0.5, 0.5,
+                  1, 1, 1,
+                  0.9, 0.9, 0.9, 0.9, 0.9, 0.45, 0.45, 0.45, 0.1, 0.1, 0.1, 0.1]
+    update_network(param_list)
+    go()"""
